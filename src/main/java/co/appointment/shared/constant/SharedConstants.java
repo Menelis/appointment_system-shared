d98ab146cf0adb4254128c929896676eab4dd6d9
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-27 01:25:00 +0200

    Remove redundant code

--- src/main/java/co/appointment/shared/constant/SharedConstants.java
+++ src/main/java/co/appointment/shared/constant/SharedConstants.java
@@ -4,13 +4,13 @@ public interface SharedConstants {
     /**
      * AES Encryption Key size
      */
-    public static final Integer AES_KEY_SIZE = 256;
+    Integer AES_KEY_SIZE = 256;
     /**
      * AES Algorithm Constant
      */
-    public static final String AESAlgorithm = "AES";
+    String AESAlgorithm = "AES";
     /**
      * AES Cipher Padding Instance
      */
-    public static final String AESCipherInstance = String.format("%s/ECB/PKCS5PADDING", AESAlgorithm);
+    String AESCipherInstance = String.format("%s/ECB/PKCS5PADDING", AESAlgorithm);
 }
