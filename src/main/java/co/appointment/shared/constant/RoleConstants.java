ba8bf96132870403d27b7ed060c8a3c39c258f7d
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-30 01:47:31 +0200

    Added RoleConstants to be shared on the system

---
+++ src/main/java/co/appointment/shared/constant/RoleConstants.java
@@ -0,0 +1,19 @@
+package co.appointment.shared.constant;
+
+/**
+ * Role Constants.
+ */
+public interface RoleConstants {
+    /**
+     * Role for customers.
+     */
+    String CUSTOMER_ROLE = "CUSTOMER";
+    /**
+     * Role for users working on the branch.
+     */
+    String USER_ROLE = "USER";
+    /**
+     * Role for admins.
+     */
+    String ADMIN_ROLE = "ADMINISTRATOR";
+}
