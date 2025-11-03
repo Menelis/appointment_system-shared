22fcca1cf1f024f3c6dfb5ee6b54dc21cc45b6b3
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-04 01:28:00 +0200

    Created class UserDetailsImpl

---
+++ src/main/java/co/appointment/shared/security/UserDetailsImpl.java
@@ -0,0 +1,68 @@
+package co.appointment.shared.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.UUID;
+
+public class UserDetailsImpl implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    private final UUID id;
+
+    @Getter
+    private final String firstName;
+
+    @Getter
+    private final String lastName;
+
+    @Getter
+    private final String email;
+
+    @Getter
+    private final String contactNo;
+
+    @JsonIgnore
+    private final String password;
+
+    private final Collection<? extends GrantedAuthority> authorities;
+
+
+    public UserDetailsImpl(
+            final UUID id,
+            final String firstName,
+            final String lastName,
+            final String email,
+            final String contactNo,
+            final String password,
+            final Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.contactNo = contactNo;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
