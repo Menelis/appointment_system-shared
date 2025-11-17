e914aa43cf58b8669d249e36b6cdeca0466c65d6
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-17 18:38:52 +0200

    Add constants for claims

--- src/main/java/co/appointment/shared/constant/TokenConstants.java
+++ src/main/java/co/appointment/shared/constant/TokenConstants.java
@@ -2,8 +2,9 @@ package co.appointment.shared.constant;
 
 public interface TokenConstants {
     String EMAIL = "email";
-    String FULL_NAME = "fullName";
+    String FULL_NAME = "given_username";
     String ROLES = "roles";
+    String USER_ID = "userId";
     String AUTHORIZATION_HEADER = "Authorization";
     String TOKEN_PREFIX = "Bearer ";
 }
