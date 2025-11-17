cc3bb78cd82c715d1cc9cb46ab1294dd67e87224
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-17 18:35:14 +0200

    Added new event types for appointment status

--- src/main/java/co/appointment/shared/constant/EventTypeConstants.java
+++ src/main/java/co/appointment/shared/constant/EventTypeConstants.java
@@ -1,6 +1,9 @@
 package co.appointment.shared.constant;
 
 public interface EventTypeConstants {
-    String EVENT_TYPE = "EventType";
-    String VERIFY_EMAIL_EVENT = "VerifyEmail";
+    String EVENT_TYPE = "eventType";
+    String VERIFY_EMAIL_EVENT = "verifyEmail";
+    String BOOKING_CONFIRMED_EVENT = "bookingConfirmed";
+    String BOOKING_PENDING_CONFIRMED_EVENT = "bookingPendingConfirmed";
+    String BOOKING_CANCELLED_EVENT = "bookingCancelled";
 }
