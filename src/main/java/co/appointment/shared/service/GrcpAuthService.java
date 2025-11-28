b1ba4c3c074933223dff7de1fe7e38ca4ecaaaeb
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-29 00:46:45 +0200

    Update service to return UserRecord instead of protobuf response GetUserResponse

--- src/main/java/co/appointment/shared/service/GrcpAuthService.java
+++ src/main/java/co/appointment/shared/service/GrcpAuthService.java
@@ -1,10 +1,22 @@
 package co.appointment.shared.service;
 
 import co.appointment.grpc.GetUserResponse;
+import co.appointment.shared.record.UserRecord;
 
 import java.util.UUID;
 
 public interface GrcpAuthService {
-    GetUserResponse getUserByEmail(String email);
-    GetUserResponse getUserById(UUID id);
+    /**
+     * Get User details from Auth Service via gRPC protocol that matches the provided email.
+     * @param email user email
+     * @return {@link UserRecord}
+     */
+    UserRecord getUserByEmail(String email);
+
+    /**
+     * Get User details from Auth Service via gRPC protocol that matches the provided id.
+     * @param id user id
+     * @return {@link UserRecord}
+     */
+    UserRecord getUserById(UUID id);
 }
--- src/main/java/co/appointment/shared/service/GrcpAuthServiceBase.java
+++ src/main/java/co/appointment/shared/service/GrcpAuthServiceBase.java
@@ -4,6 +4,7 @@ import co.appointment.grpc.AuthServiceGrpc;
 import co.appointment.grpc.GetUserByEmailRequest;
 import co.appointment.grpc.GetUserByIdRequest;
 import co.appointment.grpc.GetUserResponse;
+import co.appointment.shared.record.UserRecord;
 
 import java.util.UUID;
 
@@ -15,16 +16,22 @@ public abstract class GrcpAuthServiceBase {
     public GrcpAuthServiceBase(final AuthServiceGrpc.AuthServiceBlockingStub authServiceBlockingStub) {
         this.authServiceBlockingStub = authServiceBlockingStub;
     }
-    public GetUserResponse getUserByEmail(final String email) {
+    public UserRecord getUserByEmail(final String email) {
         GetUserByEmailRequest request = GetUserByEmailRequest.newBuilder()
                 .setEmail(email)
                 .build();
-        return authServiceBlockingStub.processUserByEmail(request);
+        return getUserRecord(authServiceBlockingStub.processUserByEmail(request));
     }
-    public GetUserResponse getUserById(final UUID id) {
+    public UserRecord getUserById(final UUID id) {
         GetUserByIdRequest request = GetUserByIdRequest.newBuilder()
                 .setId(id.toString())
                 .build();
-        return authServiceBlockingStub.processUserById(request);
+        return getUserRecord(authServiceBlockingStub.processUserById(request));
+    }
+    private UserRecord getUserRecord(final GetUserResponse response) {
+        if(response == null) {
+            return null;
+        }
+        return new UserRecord(UUID.fromString(response.getId()), response.getFullName(), response.getEmail());
     }
 }
